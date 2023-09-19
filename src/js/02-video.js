7e89f172e722e13e9653cb7804ba0e3a4a89bd54
direct66612 <direct66612@gmail.com>
2023-09-19 02:37:28 -0400

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const onPlay = time =>
+  localStorage.setItem('videoplayer-current-time', time.seconds);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+currentTime ? player.setCurrentTime(currentTime) : null;
+
+player.on('timeupdate', throttle(onPlay, 1000));
